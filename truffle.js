0766e623b6e4c894f19cb961cca2e7ceb385926a
Vitali Hurski <vitalihurski@Vitalis-MacBook-Pro.local>
2018-06-15 12:39:20 +0300

    Add a rinkeby network to config

--- truffle.js
+++ truffle.js
@@ -29,17 +29,22 @@ module.exports = {
         development: {
             host: "127.0.0.1",
             port: 7545,
-            network_id: "5777"
+            network_id: 5777
         },
         main: {
             host: "127.0.0.1",
             port: 8545,
-            network_id: "1"
+            network_id: 1
         },
         ropsten: {
             host: "127.0.0.1",
             port: 8545,
-            network_id: "3"
-        }
+            network_id: 3
+        },
+        rinkeby: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: 4
+        }        
     }
 };
