572ffc232b8dcd3e7ddf5f55ee7478f43e38fcd7
Vitali Hurski <vg@snark.art>
2018-09-25 17:45:33 +0300

    Translate comments

--- contracts/SnarkLoan.sol
+++ contracts/SnarkLoan.sol
@@ -25,8 +25,8 @@ contract SnarkLoan is Ownable, SnarkDefinitions {
         _storage = storageAddress;
     }
 
-    // в аренду может запросить заинтересованный чел
-    // в аренду может взять snark
+    // Loan can be requested by user 
+    // Loan can also be requested by Snark
     function createLoan(
         uint256[] artworksIds,
         uint256 startDate,
@@ -35,7 +35,7 @@ contract SnarkLoan is Ownable, SnarkDefinitions {
         public 
         payable
     {
-        // создаем новый запись об аренде
+        // Create new entry for a Loan 
         uint256 loanId = _storage.createLoan(
             artworksIds,
             msg.value,
@@ -44,23 +44,23 @@ contract SnarkLoan is Ownable, SnarkDefinitions {
             msg.sender
         );
 
-        // деньги переводим на счет контракта
+        // Transfer money funds into the contract 
         if (msg.value > 0) _storage.addPendingWithdrawals(_storage, msg.value);
 
-        // выставляем автоматически согласие для тех токенов,
-        // у которых выставлены свойста автоматического принятия запросов на аренду
+        // Enter automatic accept for those tokens,
+        // that agreed to automatic Loan acceptance 
         bool isAgree = false;
         for (uint256 i = 0; i < artworksIds.length; i++) {
             isAgree = (msg.sender == owner) ? 
                 _storage.isArtworkAcceptOfLoanRequestFromSnark(artworksIds[i]) :
                 _storage.isArtworkAcceptOfLoanRequestFromOthers(artworksIds[i]);
-            // проверить занятость токена... изменить его тип можно только, если он не продается,
-            // т.е. если нет оффера или аукциона и не задействован в другом loan
+            // Check status of the token ... change of token status is only possible if it is not for sale,
+            // Another words, if there is no Offer, no Auction, or no existing Loan, then change of status is possible
             uint256 saleType = _storage.getSaleTypeToArtwork(artworksIds[i]);
             if (isAgree && saleType == uint256(SaleType.None)) {
-                // !!! проверить на количество дней, на которые уже был сдан токен
-                // и в случае, если он превышает заданное количество - отклонить !!!
-                // !!! ВИДИМО ЛУЧШЕ ДЕЛАТЬ ЭТУ ПРОВЕРКУ ЕЩЕ НА ЭТАПЕ СОЗДАНИЯ ЛОАНА НА Frontend-е
+                // !!! We may need to check the number of days that the token has already been loaned 
+                // !!! and if the number of days exceeds the agreed number, then decline the Loan request 
+                // !!! Perhaps we should do this check on the front-end during Loan Creation
                 _acceptLoan(loanId, artworksIds[i], _storage.getOwnerOfArtwork(artworksIds[i]));
             }
         }
@@ -80,11 +80,11 @@ contract SnarkLoan is Ownable, SnarkDefinitions {
         emit LoanAccepted(msg.sender, _loanId, artworkId);
     }
 
-    // только система может запустить лоан в действие по расписанию
+    // Only the contract can initiate loan according to schedule 
     function startLoan(uint256 loanId) public onlyOwner {
-        // подсчитываем стоимость одной картины
+        // Get loan price of the Artwork
         uint256 _price = _storage.getLoanPriceOfArtwork(loanId);
-        // пробегаемся по всем токенам в лоане и смотрим акцепнут он или нет
+        // Check across all tokens if the Loan has been accepted
         uint256 _totalNumberOfArtworks = _storage.getTotalNumberOfLoanArtworks(loanId);
         uint256 _artworkId;
         bool _isAccepted;
@@ -93,48 +93,48 @@ contract SnarkLoan is Ownable, SnarkDefinitions {
             _artworkId = _storage.getArtworkFromLoanList(loanId, i);
             _isAccepted = _storage.isArtworkAcceptedForLoan(loanId, _artworkId);
             if (_isAccepted) {
-                // если да, то производим трансфер 
+                // if Accepted, perform transfer
                 _storage.transferArtwork(
                     _artworkId, 
                     _storage.getOwnerOfArtwork(_artworkId), 
                     _storage.getDestinationWalletOfLoan(loanId)
                 );
-                // и переводим бабло владельцам за аренду
+                // Transfer funds for the Loan to the token owners that accepted Loan
                 if (_price > 0) {
                     _currentOwnerOfArtwork = _storage.getCurrentArtworkOwnerForLoan(loanId, _artworkId);
                     _storage.subPendingWithdrawals(_storage, _price);
                     _storage.addPendingWithdrawals(_currentOwnerOfArtwork, _price);
                 }
-                // !!!!  при это необходимо перешифровать картины !!!!
-                // !!!! вероятно это делать надо со стороны backend-а по приходу события, 
-                // а не здесь и сейчас !!!!
+                // !!! We may need to reencrypt the tokens !!!!
+                // !!! Perhaps we can do this on the back-end, 
+                // !!! and not here and now !!!!
             } else {
-                // тут надо вернуть сумму за картину обратно, т.к. запрос был не принят
+                // Return funds for Art Loans that have not been accepted.
                 if (_price > 0) {
                     address _borrower = _storage.getDestinationWalletOfLoan(loanId);
                     _storage.subPendingWithdrawals(_storage, _price);
                     _storage.addPendingWithdrawals(_borrower, _price);
 
-                    // уменьшаем общую стоимость лоана
+                    // Reduce total price of Loan
                     uint256 totalPrice = _storage.getTotalPriceOfLoan(loanId);
                     _storage.setTotalPriceOfLoan(loanId, totalPrice - _price);
 
-                    // удаляем из списка
+                    // Delete entry
                     _storage.deleteArtworkFromListOfLoan(loanId, _artworkId);
                     _storage.deleteLoanToArtwork(_artworkId);
                 }
             }
         }
-        // помечаем, что Loan уже включился и работает
+        // Mark that the Loan is active and working
         _storage.setLoanSaleStatus(loanId, uint256(SaleStatus.Active));
         
         emit LoanStarted(loanId);
     }
 
-    // только система может остановить лоан по расписанию
+    // Only contract can end loan according to schedule
     function stopLoan(uint256 loanId) public onlyOwner {
         _storage.setLoanSaleStatus(loanId, uint256(SaleStatus.Finished));
-        // перемещаем все лоаны на место
+        // Transfer all loans back into place
         uint256 _totalNumberOfArtworks = _storage.getTotalNumberOfLoanArtworks(loanId);
         address _borrower = _storage.getDestinationWalletOfLoan(loanId);
         uint256 _artworkId;
@@ -149,19 +149,19 @@ contract SnarkLoan is Ownable, SnarkDefinitions {
         emit LoanFinished(loanId);
     }
 
-    // вызвать может только владелец токена
+    // Ability to terminate Loan can be called only by the token owner
     function cancelLoanArtwork(uint256 artworkId) public payable {
         address _ownerOfArtwork = _storage.getOwnerOfArtwork(artworkId);
         address _borrower = _storage.getDestinationWalletOfLoan(_loanId);
         require(msg.sender == _ownerOfArtwork, "Only an artwork owner can accept a loan request.");
 
-        // убедиться, что лоан сейчас запущен. если нет, то выход из функции
+        // Check if the loan is active, otherwise end function
         uint256 _loanId = _storage.getLoanByArtwork(artworkId);
         uint256 _status = _storage.getLoanSaleStatus(_loanId);
         require(_status == uint256(SaleStatus.Active), "Loan has to be in 'active' status");
 
-        // проверяем сумму, которая была перечислена. Если она меньше той, 
-        // что выходит на одну картину для лоана - выход с ошибкой
+        // Check amount that has been transferred.  If it is less than  
+        // amount for one artwork for loan - exit
         uint256 _price = _storage.getLoanPriceOfArtwork(_loanId);
         require(msg.value >= _price, "Payment has to be equal to cost of loan artwork");
         if (_price > 0) {
@@ -171,7 +171,7 @@ contract SnarkLoan is Ownable, SnarkDefinitions {
             _storage.addPendingWithdrawals(msg.sender, (msg.value - _price));
         }
 
-        // удалить artwork из списка loan
+        // Remove artwork token from loan entry
         _storage.deleteArtworkFromListOfLoan(_loanId, artworkId);
         _storage.deleteLoanToArtwork(artworkId);
 
@@ -182,13 +182,11 @@ contract SnarkLoan is Ownable, SnarkDefinitions {
         emit LoanOfArtworkCanceled(_loanId, artworkId);
     } 
 
-    // автоматическая отдача на уровне токена (при покупке)
-    // можно всегда изменить accept artloans
+    // Automatic function on token level 
+    // Ability to accept artloans
     function _acceptLoan(uint256 loanId, uint256 artworkId, address artworkOwner) internal {
         _storage.setSaleTypeToArtwork(artworkId, uint256(SaleType.Loan));
         _storage.acceptArtworkForLoan(loanId, artworkId);
         _storage.setCurrentArtworkOwnerForLoan(loanId, artworkId, artworkOwner);
     }
-
-
 }
