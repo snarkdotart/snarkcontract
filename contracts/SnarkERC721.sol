3465657bb4c18a98afca9b2fab0c1992cd7268f0
Vitali Hurski <vg@snark.art>
2018-10-04 00:56:02 +0300

    Fix error in tokenByIndex function

--- contracts/SnarkERC721.sol
+++ contracts/SnarkERC721.sol
@@ -81,7 +81,7 @@ contract SnarkERC721 is Ownable, SupportsInterfaceWithLookup, ERC721Basic, ERC72
 
     function tokenByIndex(uint256 _index) public view returns (uint256) {
         require(_index < totalSupply());
-        return _index;
+        return _index + 1;
     }
 
     /*****************/
