a801291f784be94caa4dea4ae70242434ede2bdb
mlsnark <39313616+mlsnark@users.noreply.github.com>
2018-10-11 14:22:33 -0400

    Update SnarkDefinitions.sol
    Updates comments for better clarity.

--- contracts/SnarkDefinitions.sol
+++ contracts/SnarkDefinitions.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.4.24;
 
 contract SnarkDefinitions {
 
-    // There are 4 states for an Offer and an Auction:
+    // There are 4 states for an Offer:
     // Preparing -recently created and not approved by participants
     // NotActive - created and approved by participants, but is not yet active (auctions only) 
     // Active - created, approved, and active 
     // Finished - finished when the artwork has sold 
     enum SaleStatus { Preparing, NotActive, Active, Finished }
 
-    // Sale type (none, offer sale, auction, art loan)
+    // Sale type (none, offer sale, art loan)
     enum SaleType { None, Offer, Auction, Loan }
 }
