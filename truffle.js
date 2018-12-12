25737fd03cb3745347582933d5e352f91a6accbb
Vitali Hurski <vg@snark.art>
2018-12-12 18:17:14 +0300

    Add a possibility to use infura provider

--- truffle.js
+++ truffle.js
@@ -1,15 +1,17 @@
 // require('dotenv').config();
 
-// var Web3 = require("web3");
-// var web3 = new Web3();
+var Web3 = require("web3");
+var web3 = new Web3();
+
 // var WalletProvider = require("truffle-hdwallet-provider");
 // var Wallet = require('ethereumjs-wallet');
 
 // var privateKey = new Buffer(process.env.SECRET_KEY, "hex");
 // var wallet = Wallet.fromPrivateKey(privateKey);
-// var mainNetProvider = new WalletProvider(wallet, "wss://mainnet.infura.io/v3/" + process.env.PROJECT_ID);
-// var ropstenProvider = new WalletProvider(wallet, "wss://ropsten.infura.io/v3/" + process.env.PROJECT_ID);
-// var rinkebyProvider = new WalletProvider(wallet, "wss://rinkeby.infura.io/v3/" + process.env.PROJECT_ID);
+
+// var mainNetProvider = new WalletProvider(wallet, "https://mainnet.infura.io/v3/" + process.env.PROJECT_ID);
+// var ropstenProvider = new WalletProvider(wallet, "https://ropsten.infura.io/v3/" + process.env.PROJECT_ID);
+// var rinkebyProvider = new WalletProvider(wallet, "https://rinkeby.infura.io/v3/" + process.env.PROJECT_ID);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -47,26 +49,26 @@ module.exports = {
         },
         main: {
             // provider: mainNetProvider,
-            // gas: 4600000,
-            // gasPrice: web3.toWei("20", "gwei"),
+            gas: 4700000,
+            gasPrice: web3.toWei("8", "gwei"),
             network_id: 1,
-            host: "127.0.0.1",
+            host: "3.16.78.59",
             port: 8545,
             from: '0xc5a3d99e05c39a18d6342b5f27c08c64a486df00'
         },
         ropsten: {
             // provider: ropstenProvider,
-            // gas: 4600000,
-            // gasPrice: web3.toWei("20", "gwei"),            
+            gas: 4700000,
+            gasPrice: web3.toWei("8", "gwei"),
             network_id: 3,
-            host: "127.0.0.1", // 3.16.78.59
+            host: "13.58.178.26",
             port: 8545,
             from: '0xc5a3d99e05c39a18d6342b5f27c08c64a486df00'
         },
         rinkeby: {
             // provider: rinkebyProvider,
-            // gas: 4600000,
-            // gasPrice: web3.toWei("20", "gwei"),
+            gas: 4700000,
+            gasPrice: web3.toWei("8", "gwei"),
             network_id: 4,
             host: "127.0.0.1",
             port: 8545,
