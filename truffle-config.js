9deb645d9e279ba7bae057c9d8206b92d2ebc64b
Vitali Hurski <vg@snark.art>
2019-02-08 18:33:00 +0300

    Update config to work with infura

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
 
 var Web3 = require("web3");
-var web3 = new Web3();
+var web3 = new Web3('http://localhost:8545');
 
-// var WalletProvider = require("truffle-hdwallet-provider");
-// var Wallet = require('ethereumjs-wallet');
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
-// var privateKey = new Buffer(process.env.SECRET_KEY, "hex");
-// var wallet = Wallet.fromPrivateKey(privateKey);
+var mainNetProvider = new HDWalletProvider(process.env.SECRET_KEY, "https://mainnet.infura.io/v3/" + process.env.PROJECT_ID);
+var ropstenProvider = new HDWalletProvider(process.env.SECRET_KEY, "https://ropsten.infura.io/v3/" + process.env.PROJECT_ID);
+var rinkebyProvider = new HDWalletProvider(process.env.SECRET_KEY, "https://rinkeby.infura.io/v3/" + process.env.PROJECT_ID);
 
-// var mainNetProvider = new WalletProvider(wallet, "https://mainnet.infura.io/v3/" + process.env.PROJECT_ID);
-// var ropstenProvider = new WalletProvider(wallet, "https://ropsten.infura.io/v3/" + process.env.PROJECT_ID);
-// var rinkebyProvider = new WalletProvider(wallet, "https://rinkeby.infura.io/v3/" + process.env.PROJECT_ID);
+console.log('Provider is: ', ropstenProvider);
 
 module.exports = {
     networks: {
@@ -22,31 +20,31 @@ module.exports = {
             gasPrice: 1
         },
         main: {
-            // provider: mainNetProvider,
+            provider: mainNetProvider,
             gas: 7800000,
             gasPrice: web3.utils.toWei("8", "gwei"),
             network_id: 1,
-            host: "18.220.154.113",
-            port: 8545,
-            from: '0xc5a3d99e05c39a18d6342b5f27c08c64a486df00'
+            // host: "18.220.154.113",
+            // port: 8545,
+            // from: '0xc5a3d99e05c39a18d6342b5f27c08c64a486df00'
         },
         ropsten: {
-            // provider: ropstenProvider,
+            provider: ropstenProvider,
             gas: 7800000,
             gasPrice: web3.utils.toWei("8", "gwei"),
             network_id: 3,
-            host: "13.58.178.26",
-            port: 8545,
-            from: '0xc5a3d99e05c39a18d6342b5f27c08c64a486df00'
+            // host: "13.58.178.26",
+            // port: 8545,
+            // from: '0xc5a3d99e05c39a18d6342b5f27c08c64a486df00'
         },
         rinkeby: {
-            // provider: rinkebyProvider,
+            provider: rinkebyProvider,
             gas: 7800000,
             gasPrice: web3.utils.toWei("8", "gwei"),
             network_id: 4,
-            host: "127.0.0.1",
-            port: 8545,
-            from: '0xc5a3d99e05c39a18d6342b5f27c08c64a486df00'
+            // host: "127.0.0.1",
+            // port: 8545,
+            // from: '0xc5a3d99e05c39a18d6342b5f27c08c64a486df00'
         }        
     },
     compilers: {
@@ -59,8 +57,5 @@ module.exports = {
                 }
             }
         },
-    },
-    mocha: {
-        // timeout: 100000
     },
 };
